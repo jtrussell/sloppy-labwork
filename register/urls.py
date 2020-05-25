fe7b99187497c84bacc7ec3177252a5fbedeecf9
jtrussell <jus.russell@gmail.com>
2020-05-25 20:06:35 +0900

    Slimmer register detail url :)

--- register/urls.py
+++ register/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.RegisterList.as_view(), name='register-home'),
     path('about/', views.about, name='register-about'),
-    path('detail/<int:pk>/', views.RegisterDetail.as_view(), name='register-detail'),
+    path('<int:pk>/', views.RegisterDetail.as_view(), name='register-detail'),
     path('new/', views.add, name='register-add'),
     path('success/', views.add, name='register-add-success'),
     path('verify/<int:id>/', views.verify, name='register-verify'),
