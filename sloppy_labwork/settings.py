463279d7c1752e2caaa69133e9077b69a4870590
jtrussell <jus.russell@gmail.com>
2021-07-28 10:42:46 -0400

    Move static root

--- sloppy_labwork/settings.py
+++ sloppy_labwork/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Feature Flags
 FT_USE_REGISTER = os.environ['FT_USE_REGISTER'] == 'True'
