d195a937afd46bab583dd9a3ae60b548307187dc
Bartosz Kijanka <bkijanka2@gmail.com>
2023-01-15 22:10:22 +0100

    woe-simulator
    Basic implementation finished

--- deck_simulator/urls.py
+++ deck_simulator/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.deck_simulator, name='deck-simulator'),
-    path('<str:id>/', views.deck_info),
-    path('my-decks', views.my_decks),
-    path('proxy', views.proxy, name='deck-proxy'),
+    path('<str:id>/', views.deck_info, name='generated-deck-page'),
+    path('my-decks', views.my_decks, name='my-generated-decks'),
+    path('<str:id>/proxy', views.proxy, name='generated-deck-proxy'),
 ]
