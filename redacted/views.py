cbd5d88cb4219d91e1578975784baadc9e9810d1
Justin Russell <justin.russell@clublocker.com>
2024-02-05 06:35:59 -0500

    Replace shard of knowledge with link to server

--- redacted/views.py
+++ redacted/views.py
@@ -180,9 +180,5 @@ def random_access_archives(request):
     })
 
 
-def shard_of_knowledge(request):
-    return render(request, 'redacted/shard-of-knowledge.html')
-
-
 def list(request):
     return render(request, 'redacted/list.html')
