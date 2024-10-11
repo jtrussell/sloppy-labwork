6677a938e712631fef64ae0e1e94a84ae77a276a
Justin Russell <justin.russell@clublocker.com>
2024-10-11 06:33:16 -0400

    Split out templates for TP state machine

--- transporter_platform/views.py
+++ transporter_platform/views.py
@@ -2,6 +2,28 @@ from django.shortcuts import render
 from allauth.account.decorators import login_required
 
 
-@login_required
+# @login_required
 def kagi_live(request):
-    return render(request, 'transporter_platform/kagi_live.html', {})
+    is_checked_in = True
+    matched_with = 'The Villain'
+
+    if matched_with:
+        return _kagi_live_matched(request, matched_with)
+    elif is_checked_in:
+        return _kagi_live_check_waiting_for_match(request)
+    else:
+        return _kagi_live_check_in(request)
+
+
+def _kagi_live_check_in(request):
+    return render(request, 'transporter_platform/page-kagi-live--check-in.html', {})
+
+
+def _kagi_live_check_waiting_for_match(request):
+    return render(request, 'transporter_platform/page-kagi-live--waiting.html', {})
+
+
+def _kagi_live_matched(request, matched_with):
+    return render(request, 'transporter_platform/page-kagi-live--matched.html', {
+        'matched_with': matched_with
+    })
