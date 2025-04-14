401cb48c2b22ebc38fef89d6f1c19f2d45aa690f
Justin Russell <justin.russell@clublocker.com>
2025-04-14 17:49:14 -0400

    Poke heroku

--- sloppy_labwork/settings.py
+++ sloppy_labwork/settings.py
@@ -179,9 +179,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
