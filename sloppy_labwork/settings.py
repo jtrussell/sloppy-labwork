f83a8305611880fb462b7c35c5f3a520782fe93a
Justin Russell <justin.russell@clublocker.com>
2025-04-20 21:14:22 -0400

    ft: Add Google auth provider

--- sloppy_labwork/settings.py
+++ sloppy_labwork/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.discord',
     'register',
     'ratings',
