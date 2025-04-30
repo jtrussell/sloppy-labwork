bec850a35ff65c77aafdbb11a0a4e03fadbcfee0
Justin Russell <justin.russell@clublocker.com>
2025-04-29 21:20:04 -0400

    fx: Slashify common URLs

--- common/urls.py
+++ common/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('privacy', views.privacy, name='privacy'),
-    path('terms', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('terms/', views.terms, name='terms'),
 ]
