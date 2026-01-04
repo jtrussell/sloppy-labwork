26f09feb94e80f6ba100f46512d5566d5f2e6781
Justin Russell <justin.russell@clublocker.com>
2026-01-04 06:25:26 -0500

    Add gizmo link for timers

--- timekeeper/urls.py
+++ timekeeper/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('<str:timer_code>/start/', views.timer_start, name='timer_start'),
     path('<str:timer_code>/pause/', views.timer_pause, name='timer_pause'),
     path('<str:timer_code>/add/', views.timer_add_time, name='timer_add_time'),
-    path('<str:timer_code>/subtract/', views.timer_subtract_time, name='timer_subtract_time'),
+    path('<str:timer_code>/subtract/',
+         views.timer_subtract_time, name='timer_subtract_time'),
     path('<str:timer_code>/set/', views.timer_set_time, name='timer_set_time'),
     path('<str:timer_code>/delete/', views.timer_delete, name='timer_delete'),
 ]
